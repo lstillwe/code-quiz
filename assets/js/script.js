2048e5585b0ea6c223939f13208514f71bb8c7d0
Lisa Stillwell <lisa@renci.org>
2021-10-27 16:01:41 -0400

    added button styling and more layout design

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,15 +32,79 @@ quizArray.push(quizQandA);
 var startQuizText = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!";
 var quizLen = quizArray.length;
 
-var titleEl = document.getElementById("title-paragraph");
-var paragraphText = titleEl.childNodes[0];
-paragraphText.data = startQuizText;
+// get main content div for control of content
+var contentDivEl = document.querySelector(".content");
+// get div for title text
+var divTitleEl = document.querySelector("#title");
+// get div for quiz buttons
+var divButtonsEl = document.querySelector("#buttons");
+// get div for any quiz responses
+var divResponseEl = document.querySelector("#response");
 
-var q1Buttons = [];
+createStartQuizContent();
 
-
+// clear all children from the divs defined in the <content> section
+function clearContentDivs() {
+    divTitleEl.innerHTML = "";
+    divButtonsEl.innerHTML = "";
+    divResponseEl.innerHTML = "";
+}
 
 function startQuiz() {
+    for (var i = 0; i <quizLen; i++) {
 
+        createQuizQuestionContent(quizArray[i])
 
+    }
+}
+
+function createStartQuizContent() {
+    // remove previous content
+    clearContentDivs();
+
+    // Add quiz instructions and start button
+    // add paragraph to the title div
+    var paragraphEl = document.createElement("p");
+    paragraphEl.setAttribute("id",  "title-paragraph");
+    paragraphEl.textContent = startQuizText;
+    divTitleEl.appendChild(paragraphEl);
+
+    // add button to the buttons div
+    var buttonEl = document.createElement("button");
+    buttonEl.setAttribute("id",  "start-button");
+    buttonEl.setAttribute("type",  "button");
+    buttonEl.textContent = "Start Quiz";
+    divButtonsEl.appendChild(buttonEl);
+
+    buttonEl.addEventListener("click", startQuiz);
+}
+
+function createQuizQuestionContent(qObject) {
+
+    clearContentDivs()
+    // Add quiz instructions and start button
+    // add paragraph to the title div
+    var paragraphEl = document.createElement("p");
+    paragraphEl.setAttribute("id",  "title-paragraph");
+    paragraphEl.textContent = qObject.question;
+    divTitleEl.appendChild(paragraphEl);
+
+    // add button to the buttons div
+    var qButtons = qObject.possibleAns;
+    for (i=0; i< qButtons.length; i++) {
+        var buttonEl = document.createElement("button");
+        buttonEl.setAttribute("id",  "button" + i);
+        buttonEl.setAttribute("type",  "button");
+        buttonEl.textContent = qButtons[i];
+        divButtonsEl.appendChild(buttonEl);
+
+        // buttonEl.addEventListener("click", startQuiz);
+    }
+    
+}
+
+function createDoneContent() {
+}
+
+function createHighScoreContent() {
 }
